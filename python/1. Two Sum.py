a5a08ba48c3b0adde366aa5a53a233ee94033834
Ananda Wiradharma <anandawira754@gmail.com>
2021-07-19 19:13:29 +0700

    remove python code

--- python/1. Two Sum.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        for idx, val in enumerate(nums):
-            try:
-                idx2 = nums.index(target-val, idx+1)
-                
-                return [idx, idx2]
-            except Exception:
-                pass
