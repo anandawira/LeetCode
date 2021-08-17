77d4a86b049e9be4600d8d8bd39f6285f3808f94
Ananda Wiradharma <anandawira754@gmail.com>
2021-08-18 00:35:00 +0700

    daily

---
+++ typescript/Count Good Nodes in Binary Tree.ts
@@ -0,0 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+class TreeNode {
+  val: number;
+  left: TreeNode | null;
+  right: TreeNode | null;
+  constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.left = left === undefined ? null : left;
+    this.right = right === undefined ? null : right;
+  }
+}
+
+function goodNodes(root: TreeNode | null): number {
+  let goodCount: number = 0;
+
+  const checkChildren = (node: TreeNode, max: number): void => {
+    if (node === null) return;
+
+    if (node.val >= max) {
+      goodCount++;
+      max = node.val;
+    }
+
+    checkChildren(node.left, max);
+    checkChildren(node.right, max);
+  };
+
+  checkChildren(root, root.val);
+
+  return goodCount;
+}
