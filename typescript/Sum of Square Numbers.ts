831a52aa25b9123c2908cdd9a312a279b601a196
Ananda Wiradharma <anandawira754@gmail.com>
2021-08-25 21:07:46 +0700

    daily

--- typescript/Sum of Square Numbers.ts
+++ typescript/Sum of Square Numbers.ts
@@ -1,12 +1,8 @@
-/**
- * @param {number} c
- * @return {boolean}
- */
-var judgeSquareSum = function (c) {
+function judgeSquareSum(c: number): boolean {
   if (Number.isInteger(Math.sqrt(c))) return true;
 
   for (let a = 0; a * a <= c; a++) {
     if (Number.isInteger(Math.sqrt(c - a * a))) return true;
   }
   return false;
-};
+}
