a674c718c1114f530f7d67fad4505cd4eddcbcc8
Ananda Wiradharma <anandawira754@gmail.com>
2021-12-22 21:45:34 +0700

    blind go

---
+++ Blind Golang/Array/1. Two Sum.go
@@ -0,0 +1,19 @@
+package Array
+
+func twoSum(nums []int, target int) []int {
+	checkedNumbers := make(map[int]int)
+
+	for index, cur := range nums {
+		diff := target - cur
+
+
+
+		if v, ok := checkedNumbers[diff]; ok{
+			return []int{index, v}
+		}
+
+		checkedNumbers[cur] = index
+	}
+
+	return nil
+}
